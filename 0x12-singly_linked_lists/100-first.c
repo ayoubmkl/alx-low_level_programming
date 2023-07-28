e117f2777bfd9884f9e1a9e6eb5e8ce86c7b6769
ayoub mekhennet <medmek1956@gmail.com>
2023-07-27 17:11:25 -0700

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,10 +2,6 @@
 
 void first(void) __attribute__ ((constructor));
 
-/**
- * first - prints a sentence before the main
- * function is executed
- */
 void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
